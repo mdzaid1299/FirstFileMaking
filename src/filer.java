9eecce1632c9450d99ecb9a814b3fa0ce88479df
HP <mdzaid1299@gmail.com>
2022-08-29 00:22:31 +0530

    new commit

--- src/filer.java
+++ src/filer.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 public class filer {
     public static void main(String[] args) {
         try {
-            File obj1 = new File("SecoOne.txt");
-            if (obj1.createNewFile()) {
+            File obj1 = new File("3rdOne.txt");
+            obj1.createNewFile();
                 System.out.println("file created = " + obj1.getName());
                 System.out.println(" path " + "= " + obj1.getPath());
-           } else {
-                System.out.println("diktat aa agi");
-           }
+//           } else {
+//                System.out.println("diktat aa agi");
+
         } catch (IOException e) {
             System.out.println("error hai");
             e.printStackTrace();
